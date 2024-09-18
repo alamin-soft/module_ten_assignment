17cd0d956cb09d664a8f22e9fc1aac1ddf36d4cb
unknown <pro.mdamin@gmail.com>
2024-09-19 01:25:05 +0600

    SomeOfFuctionsAdded

---
+++ index.js
@@ -0,0 +1,71 @@
+
+// question no 1
+
+
+/*function calculateDifference(num1, num2) {
+    return num1 - num2;
+  }
+*/
+
+// question no 2
+
+
+ /* function isOdd(num) {
+    return num % 2 !== 0;
+  }
+*/
+
+// Question no 3
+
+ /* function findMin(array) {
+    let min = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
+      }
+    }
+    return min;
+  }
+*/
+
+// Question no 4
+
+ /* function filterEvenNumbers(array) {
+    return array.filter(num => num % 2 === 0);
+  }
+*/
+
+// Question no 5
+
+/*
+    function sortArrayDescending(array) {
+    return array.sort((a, b) => b - a);
+ }
+*/
+
+// Question no 6
+
+  /*  function lowercaseFirstLetter(str) {
+    return str.charAt(0).toLowerCase() + str.slice(1);
+  }
+*/
+
+// Question no 7
+
+ /* function countVowels(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    return str.split('').filter(char => vowels.includes(char)).length;
+  }
+*/
+
+Question no 8
+
+    function findAverage(array) {
+        const sum = array.reduce((acc, curr) => acc + curr, 0);
+        return sum / array.length;
+  }
+
+
+//   End of Module Ten assignment 
+
+
